61ae684763dd09dcc39cc284c4c42f1f4b25b090
loha2123 <91543308+loha2123@users.noreply.github.com>
2021-10-11 14:30:30 +0500

    Update compress.py

--- compress.py
+++ compress.py
@@ -2,29 +2,6 @@
 #removed making the user to run commands for audio and video
 #added import signal for keyboard interrupt
 
-import os
-import ffmpeg
-import math
-import time
-from pymediainfo import MediaInfo
-import signal
-
-def keyboardInterruptHandler(signal, frame):
-    print(" ")
-    print(" ")
-    print("Compresor closing...".format(signal))
-    exit(0)
-
-signal.signal(signal.SIGINT, keyboardInterruptHandler)
-
-print("8MB File Compressor for Discord (and others)")
-video = input("Please input your video file name. It should be located in the same directory as this script: ")
-audio_kbps = input("What audio length would you like to have in the output? (Default: 96kbps): ")
-if audio_kbps == "":
-    audio_kbps = 96
-user_os = input("What's your OS? Input " + "w " + "for Windows and " + "l " + "for Linux. (w/l): ")
-file_size = input("What output file size would you like? (in megabytes) (default: 8): ")
-if file_size == "":
     file_size = 8
 print("")
 print("*** Please, read the following info ***")
@@ -36,18 +13,8 @@ print("AV1 is the best codec right now, but, Discord can't play it either.")
 print("")
 print("Available options: 1 for VP9, 2 for H264, 3 for H265, 4 for AV1.")
 codec = input("What codec would you like? (default: VP9): ")
-if codec == "":
-    codec = "libvpx-vp9"
-if codec == "1":
-    codec = "libvpx-vp9"
-if codec == "2":
-    codec = "libx264"
-if codec == "3":
-    codec = "libx265"
-if codec == "4":
-    codec = "libaom-av1"
-print("You will be prompted to choose the video compression preset. Recommended: veryslow. You can choose medium too, fast compression, worse quality. Placebo is not needed, it dosen't vary from veryslow that much.")
-preset = input("What video preset would you like? (Options: ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, placebo): ")
+if 
+fast, medium, slow, slower, veryslow, placebo): ")
 if preset != "ultrafast" or preset != "superfast" or preset != "veryfast" or preset != "faster" or preset != "fast" or preset != "medium" or preset != "slow" or preset != "slower" or preset != "veryslow" or preset != "placebo":
     print("You prompted nothing / something that is not in the list. Preset will set as veryslow as default.")
     preset = "veryslow"
